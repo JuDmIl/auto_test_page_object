a4116ddba3636f674d4c6313a301998f00ebfa02
JuDmIl <ju-ilyina@yandex.ru>
2023-12-01 22:22:31 +0300

    Создана заготовка для теста на основе Page Object

---
+++ pages/main_page.py
@@ -0,0 +1,9 @@
+# импорт базового класса BasePage
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+
+# Класс MainPage - наследник класса BasePage
+class MainPage(BasePage):
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link.click() 
